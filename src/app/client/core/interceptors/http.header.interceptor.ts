3cea0b451aadeb29efc6dd93a2b07d95982e8eff
Ahmed Elkenawy <ahmed.elkenawy95@gmail.com>
2019-06-01 12:34:35 +0200

    responsive style

--- src/app/client/core/interceptors/http.header.interceptor.ts
+++ src/app/client/core/interceptors/http.header.interceptor.ts
@@ -7,20 +7,25 @@ import { Observable } from 'rxjs';
 @Injectable({
     providedIn: 'root'
   })
+   /**
+     * inercept the request and add header 
+         * add the token as a header 
+    */
 export class HeaderInterceptor implements HttpInterceptor{
     intercept(req : HttpRequest<any> , next : HttpHandler ) : Observable<HttpEvent<any>>{
-       
+      
         const  token = localStorage.getItem('token') ;
         const httpHeader = {
         
               'Content-Type':  'application/json' 
               
             }
+           
  
         if (token) {
             httpHeader['Authorization'] =`Token ${token}` ;
        }
-        
+       //change the request add add the new header  
        const request  = req.clone( 
         {setHeaders : httpHeader }
         )
